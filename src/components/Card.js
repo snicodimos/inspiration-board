d5a1c768ae8d621db4f9daa822c8d8b8614f8ea1
Semret Nicodimos <snicodimos@gmail.com>
2018-12-15 13:07:48 -0800

    added an event handler and callback for delete functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,11 +5,24 @@ import emoji from 'emoji-dictionary';
 import './Card.css';
 
 class Card extends Component {
+  deleteCardClickHandler = (event) => {
+    console.log("The delete button was clicked");
+    
+    this.props.deleteCardCallback(this.props.id);
+  }
   render() {
     return (
       <div className="card">
-        <p>{this.props.text}</p>
-        <p>{this.props.emoji}</p>
+        <div>
+          {this.props.text}
+        </div>
+        <div>
+          {this.props.emoji}
+        </div>
+        <button
+          onClick={ this.deleteCardClickHandler}>
+          Delete
+        </button>
       </div>
     )
   }
